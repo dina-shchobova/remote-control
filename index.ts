9f6f6180026502ef2294bb27c3a0f28ee5a1447b
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-22 23:55:08 +0300

    feat: add WebSocketServer

--- index.ts
+++ index.ts
@@ -7,3 +7,17 @@ const HTTP_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function message(data) {
+    console.log('Received: %s', data);
+  });
+
+  ws.send('something');
+});
+
+wss.on('close', function close() {
+  console.log('Disconnected');
+});
