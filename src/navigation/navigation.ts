1c8faeba50effbe3c346c616223134cfbe99bb35
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-23 13:53:43 +0300

    feat: add navigate

---
+++ src/navigation/navigation.ts
@@ -0,0 +1,39 @@
+import robot from 'robotjs';
+
+robot.setMouseDelay(2);
+var mouse = robot.getMousePos();
+
+const navigate = (data: string) => {
+  let command = data.split(' ');
+  let direction = command[0];
+  let step = +command[1];
+
+  switch (direction) {
+    case 'mouse_up': {
+      mouse.y -= step;
+      robot.moveMouse(mouse.x, mouse.y);
+      return 'mouse_up';
+    }
+    case 'mouse_down': {
+      mouse.y += step;
+      robot.moveMouse(mouse.x, mouse.y - step);
+      return 'mouse_down';
+    }
+    case 'mouse_left': {
+      mouse.x -= step;
+      robot.moveMouse(mouse.x - step, mouse.y);
+      return 'mouse_left';
+    }
+    case 'mouse_right': {
+      mouse.x += step;
+      robot.moveMouse(mouse.x + step, mouse.y);
+      return 'mouse_right';
+    }
+    case 'mouse_position': {
+      return `mouse_position ${mouse.x},${mouse.y}`;
+    }
+  }
+}
+
+export { navigate, mouse };
+
