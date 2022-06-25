d1274a6762e2527e4ceb0bd4e423988f13bcc641
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-25 11:55:49 +0300

    fix: fix navigation.ts, move getMousePos

--- src/navigation/navigation.ts
+++ src/navigation/navigation.ts
@@ -1,12 +1,13 @@
 import robot from 'robotjs';
 
 robot.setMouseDelay(2);
-let mouse = robot.getMousePos();
 
 const navigate = (data: string) => {
   let command = data.split(' ');
   let direction = command[0];
   let step = +command[1];
+
+  let mouse = robot.getMousePos();
 
   switch (direction) {
     case 'mouse_up': {
@@ -16,17 +17,17 @@ const navigate = (data: string) => {
     }
     case 'mouse_down': {
       mouse.y += step;
-      robot.moveMouse(mouse.x, mouse.y - step);
+      robot.moveMouse(mouse.x, mouse.y);
       return 'mouse_down';
     }
     case 'mouse_left': {
       mouse.x -= step;
-      robot.moveMouse(mouse.x - step, mouse.y);
+      robot.moveMouse(mouse.x, mouse.y);
       return 'mouse_left';
     }
     case 'mouse_right': {
       mouse.x += step;
-      robot.moveMouse(mouse.x + step, mouse.y);
+      robot.moveMouse(mouse.x, mouse.y);
       return 'mouse_right';
     }
     case 'mouse_position': {
