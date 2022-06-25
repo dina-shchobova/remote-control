ebf396b109eca5fd951c4d78ec71749205a1da48
Dina Shchobova <dina.shchobova@gmail.com>
2022-06-25 17:14:39 +0300

    refactor: refactor backend.ts

--- backend.ts
+++ backend.ts
@@ -4,17 +4,22 @@ import { navigate } from "./src/navigation/navigation.js";
 import { draw } from "./src/drawing/drawing.js";
 import { print } from "./src/printing/printing.js";
 
-const wss = new WebSocketServer({ port: 8080 });
+const PORT = 8080;
+
+const wss = new WebSocketServer({ port: PORT });
+console.log(`Start WebSocketServer on the ${PORT} port!\n`);
 
 let msgToFront: MsgToFront = "";
 
 wss.on("connection", function connection(ws) {
+  console.log(`The WebSocket is currently open\n`);
+
   ws.on("message", async function message(data) {
     let msgFromFront = data.toString();
     let indexUnderscore = msgFromFront.indexOf("_", 0);
     let verb = msgFromFront.slice(0, indexUnderscore);
     try {
-      console.log(`Сommand received: ${msgFromFront}`);
+      console.log(`Command received: ${msgFromFront}`);
       switch (verb) {
         case "mouse":
           msgToFront = navigate(msgFromFront);
@@ -26,16 +31,15 @@ wss.on("connection", function connection(ws) {
           msgToFront = await print();
           break;
       }
-      console.log(`Result: completed successfully`);
+      console.log(`Result: completed successfully\n`);
       ws.send(`${(msgToFront)}\0`);
     } catch (e) {
-      console.log(`Result: error`);
+      console.log(`Result: error\n`);
       ws.send(`${(msgFromFront)}\0`);
     }
 
   });
-});
-
-wss.on("close", function close() {
-  console.log("Disconnected");
+  ws.on("close", function close() {
+    console.log("The WebSocket is currently closed\n");
+  });
 });
